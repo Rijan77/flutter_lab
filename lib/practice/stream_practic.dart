0feaabcd0f76229323cdc48dad65f8a16a8c1383
Rijan77 <rijanacharya73@gmail.com>
2025-07-17 09:31:19 +0545

    Stream Practic

--- lib/practice/stream_practic.dart
+++ lib/practice/stream_practic.dart
@@ -1,6 +1,6 @@
 Stream<String> fetchDataStream() async* {
   for (int i = 1; i <= 3; i++) {
-    await Future.delayed(Duration(seconds: 1));
+    await Future.delayed(const Duration(seconds: 1));
     yield "Data $i"; // Yield sends a value to the stream
   }
 }
