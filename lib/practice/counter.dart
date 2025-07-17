b95549e0d7832da1e3fb8b16d2ce3f3abd50b371
Rijan77 <rijanacharya73@gmail.com>
2025-07-17 17:50:28 +0545

    Used same timer to across page

--- lib/practice/counter.dart
+++ lib/practice/counter.dart
@@ -6,45 +6,45 @@ import 'package:riverpod_state/practice/homepage.dart';
 import 'home_screen.dart';
 
 class TimerDisplayPage extends StatefulWidget {
-
   final DateTime endTime;
-  TimerDisplayPage({super .key, required this.endTime});
+
+  TimerDisplayPage({super.key, required this.endTime});
 
   @override
   _TimerDisplayPageState createState() => _TimerDisplayPageState();
-
-
 }
 
 class _TimerDisplayPageState extends State<TimerDisplayPage> {
   final DateTime sharedEndTime = DateTime.now().add(Duration(minutes: 10));
 
-
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
-     body: Center(
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.center,
-         children:  [
-           TimerCountdown(
-               format: CountDownTimerFormat.minutesSeconds,
-               onEnd: () {print("Timer finished");
-               },
-           
-                 endTime: sharedEndTime),
-
-           SizedBox(height: 50,),
-           ElevatedButton(onPressed: (){
-
-             Navigator.push(context, MaterialPageRoute(builder: (_) => CounterPage(endTime: sharedEndTime),));
-           }, child: Text("Next Page"))
-         ],
-
-       ),
-     ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TimerCountdown(
+                format: CountDownTimerFormat.minutesSeconds,
+                onEnd: () {
+                  print("Timer finished");
+                },
+                endTime: sharedEndTime),
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => CounterPage(endTime: sharedEndTime),
+                      ));
+                },
+                child: Text("Next Page"))
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/practice/home_screen.dart
+++ lib/practice/home_screen.dart
@@ -1,42 +1,45 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_timer_countdown/flutter_timer_countdown.dart';
 
 class CounterPage extends ConsumerWidget {
   final DateTime endTime;
-   CounterPage({super.key, required this.endTime});
 
-  final counterProvider = StateProvider<int>((ref){
+  CounterPage({super.key, required this.endTime});
+
+  final counterProvider = StateProvider<int>((ref) {
     return 0;
   });
-  
-  final switchProvider = StateProvider<bool>((ref){
+
+  final switchProvider = StateProvider<bool>((ref) {
     return false;
-});
+  });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {// watch the counter state
+  Widget build(BuildContext context, WidgetRef ref) {
+    // watch the counter state
     print("Build1");
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back_sharp)),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back_sharp)),
         title: Center(child: Text("Helo Timer")),
       ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TimerCountdown(
-            format: CountDownTimerFormat.minutesSeconds,
-            endTime: endTime,
-            onEnd: () => print("Timer Finished on Page 2"),
-          ),
-
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TimerCountdown(
+              format: CountDownTimerFormat.minutesSeconds,
+              endTime: endTime,
+              onEnd: () => print("Timer Finished on Page 2"),
+            ),
+          ],
+        ),
       ),
-    ),
-
     );
   }
 }
